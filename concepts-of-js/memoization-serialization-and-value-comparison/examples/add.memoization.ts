6a97e737fdfd557e70d814045fb96c0426422098
pepe_fabra_valverde <jofaval@outlook.com>
2023-04-24 17:07:35 +0200

    feat(concepts-of-js): memoize examples

--- concepts-of-js/memoization-serialization-and-value-comparison/examples/add.memoization.ts
+++ concepts-of-js/memoization-serialization-and-value-comparison/examples/add.memoization.ts
@@ -1,3 +1,20 @@
 function add(a: number, b: number): number {
   return a + b;
 }
+
+const memoizedAddCache = new Map<string, number>();
+
+function memoizedAdd(a: number, b: number): number {
+  const serializedArguments = [a, b].toString();
+
+  const cachedEntry = memoizedAddCache.get(serializedArguments);
+  if (cachedEntry) {
+    return cachedEntry;
+  }
+
+  console.log("computing...");
+
+  const result = add(a, b);
+  memoizedAddCache.set(serializedArguments, result);
+  return result;
+}
--- concepts-of-js/memoization-serialization-and-value-comparison/examples/factorial.memoization.ts
+++ concepts-of-js/memoization-serialization-and-value-comparison/examples/factorial.memoization.ts
@@ -3,6 +3,23 @@ function recursiveFactorial(n: number): number {
   return recursiveFactorial(n - 1) * n;
 }
 
+const memoizedRecursiveFactorialCache = new Map<number, number>();
+
+function memoizedRecursiveFactorial(n: number): number {
+  const cachedEntry = memoizedRecursiveFactorialCache.get(n);
+  if (cachedEntry) {
+    return cachedEntry;
+  }
+
+  console.log("computing...");
+
+  const result = recursiveFactorial(n);
+  memoizedRecursiveFactorialCache.set(n, result);
+  return result;
+}
+
+// observe how many times "computing..." gets logged for each new bigger number
+
 function factorial(n: number): number {
   if (n <= 1) return 1;
 
@@ -12,4 +29,19 @@ function factorial(n: number): number {
   }
 
   return accumulative;
+}
+
+const memoizedFactorialCache = new Map<number, number>();
+
+function memoizedFactorial(n: number): number {
+  const cachedEntry = memoizedFactorialCache.get(n);
+  if (cachedEntry) {
+    return cachedEntry;
+  }
+
+  console.log("computing...");
+
+  const result = factorial(n);
+  memoizedFactorialCache.set(n, result);
+  return result;
 }
